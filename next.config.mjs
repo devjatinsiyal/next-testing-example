23f4fcdc7238b28105676e8847bef86374720ccf
Jatin Siyal <jatinsiyal@Jatins-MacBook-Pro.local>
2024-05-14 12:28:41 +0530

    Added next config file

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+  },
+}
+
+export default nextConfig
